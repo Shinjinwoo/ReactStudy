31b432f513061ef9a6de22cf21486a2c5607aefa
shin-jinwoo <jinu712@gmail.com>
2023-01-21 11:22:07 +0900

    Commit Test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,13 @@
+
+
 import React from "react";
 
-const App = () => <div>App</div>;
+const App = () => <div>
+    안녕민지야 ㅋㅋㅋ
+
+    </div>;
+
+
+
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,3 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const Title = (
+    <h3 id = "title" onMouseEnter={() => console.log("hihihihi")}> hi </h3>
+)
